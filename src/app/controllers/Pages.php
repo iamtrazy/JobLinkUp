5037dfd0ccb47b6a64f98c1bc1f292ef11bcff3d
iamtrazy <iamtrazy@proton.me>
2023-09-25 23:44:29 +0530

    Feat: MVC framework added

---
+++ src/app/controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php
+class Pages extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function index()
+    {
+        $this->view('hello');
+    }
+
+    public function about($id)
+    {
+        echo $id;
+    }
+}
