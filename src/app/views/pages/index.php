2b3ae5efbd7302edec4a627cf66f935143ab99fb
iamtrazy <iamtrazy@proton.me>
2023-09-30 19:50:10 +0530

    feat: registration and signup (basic level)

--- src/app/views/pages/index.php
+++ src/app/views/pages/index.php
@@ -1,4 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
   <h1><?php echo $data['title']; ?></h1>
-  <p>Yet to cretae a landing page</p>
+  <p>Yet to create a landing page</p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
