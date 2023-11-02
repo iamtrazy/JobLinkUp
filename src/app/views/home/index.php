b4f7ec61c690f90be07ca83cb49376d80fc908fe
iamtrazy <iamtrazy@proton.me>
2023-11-03 04:03:40 +0530

    fix: landing page footer

--- src/app/views/home/index.php
+++ src/app/views/home/index.php
@@ -1,194 +1,137 @@
-
 <?php require APPROOT . '/views/inc/guest_header.php'; ?>
-      <div class="page-content">
-        <div class="twm-home4-banner-section site-bg-light-purple">
-          <div class="row">
-            <!--Left Section-->
-            <div class="col-xl-6 col-lg-12 col-md-12">
-              <div class="twm-bnr-left-section">
-                <div class="twm-bnr-title-large">
-                  <span class="site-text-primary">Part-Time </span> Jobs Made Simple
+<div class="page-content">
+  <div class="twm-home4-banner-section site-bg-light-purple">
+    <div class="row">
+      <!--Left Section-->
+      <div class="col-xl-6 col-lg-12 col-md-12">
+        <div class="twm-bnr-left-section">
+          <div class="twm-bnr-title-large">
+            <span class="site-text-primary">Part-Time </span> Jobs Made Simple
+          </div>
+          <div class="twm-bnr-discription">
+            Discover Part-Time Opportunities Aligned with Your Interests Here
+          </div>
+          <div class="twm-bnr-search-bar">
+            <form>
+              <div class="row">
+                <!--Title-->
+                <div class="form-group col-xl-8 col-lg-8 col-md-8">
+                  <label>What</label>
+                  <div class="twm-single-iput">
+                    <input name="username" type="text" required="" class="form-control bg-none" placeholder="Job title, Keywords, or company" />
+                  </div>
                 </div>
-                <div class="twm-bnr-discription">
-                Discover Part-Time Opportunities Aligned with Your Interests Here
-                </div>
-                <div class="twm-bnr-search-bar">
-                  <form>
-                    <div class="row">
-                      <!--Title-->
-                      <div class="form-group col-xl-8 col-lg-8 col-md-8">
-                        <label>What</label>
-                        <div class="twm-single-iput">
-                          <input
-                            name="username"
-                            type="text"
-                            required=""
-                            class="form-control bg-none"
-                            placeholder="Job title, Keywords, or company"
-                          />
-                        </div>
-                      </div>
 
-                      <!--Find job btn-->
-                      <div class="form-group col-xl-4 col-lg-4 col-md-4">
-                        <button type="button" class="site-button">
-                          Find Job
-                        </button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-                <div class="twm-bnr-popular-search">
-                  <span class="twm-title">Popular Searches:</span>
-                  <a href="#"
-                    >Freelance Writing</a
-                  >
-                  ,
-                  <a href="#"
-                    >Babysitting</a
-                  >
-                  ,
-                  <a href="#"
-                    >Remote Work</a
-                  >
-                  ,
-                  <a href="#"
-                    >Pet Care</a
-                  >
-                  ,
-                  <a href="#"
-                    >Lawn Care</a
-                  >
-                  ,
-                  <a href="#"
-                    >Barista</a
-                  >
-                  ,
-                  <a href="#"
-                    >Writing</a
-                  >
-                  ,
-                  <a href="#"
-                    >Dog Walking</a
-                  >...
+                <!--Find job btn-->
+                <div class="form-group col-xl-4 col-lg-4 col-md-4">
+                  <button type="button" class="site-button">
+                    Find Job
+                  </button>
                 </div>
               </div>
-            </div>
-
-            <!--right Section-->
-            <div class="col-xl-6 col-lg-12 col-md-12">
-              <div
-                class="twm-bnr-right-section anm"
-                data-wow-delay="1000ms"
-                data-speed-x="2"
-                data-speed-y="2"
-                style="
-                  transform: translate3d(12.34px, -7.47px, 0px) scale(1)
-                    rotate(0deg);
-                  opacity: 1;
-                "
-              >
-                <div class="twm-graphics-h3 twm-bg-line">
-                  <img src="img/bg-line.png" alt="" />
-                </div>
-
-                <div class="twm-graphics-user twm-user">
-                  <img src="img/user.png" alt="" />
-                </div>
-
-                <div class="twm-graphics-h3 twm-bg-plate">
-                  <img src="img/bg-plate.png" alt="" />
-                </div>
-
-                <div class="twm-graphics-h3 twm-checked-plate">
-                  <img src="img/checked-plate.png" alt="" />
-                </div>
-
-                <div class="twm-graphics-h3 twm-blue-block">
-                  <img src="img/blue-block.png" alt="" />
-                </div>
-
-                <div class="twm-graphics-h3 twm-color-dotts">
-                  <img src="img/color-dotts.png" alt="" />
-                </div>
-
-                <div
-                  class="twm-graphics-h3 twm-card-large anm"
-                  data-speed-y="-2"
-                  data-speed-scale="-15"
-                  data-speed-opacity="50"
-                  style="
-                    transform: translate3d(0px, 7.47px, 0px) scale(0.873319)
-                      rotate(0deg);
-                    opacity: 0.57773;
-                  "
-                >
-                  <img src="img/card.png" alt="" />
-                </div>
-
-                <div
-                  class="twm-graphics-h3 twm-card-s1 anm"
-                  data-speed-y="2"
-                  data-speed-scale="15"
-                  data-speed-opacity="50"
-                  style="
-                    transform: translate3d(0px, -7.47px, 0px) scale(1.12668)
-                      rotate(0deg);
-                    opacity: 0.57773;
-                  "
-                >
-                  <img src="img/card-s1.png" alt="" />
-                </div>
-
-                <div
-                  class="twm-graphics-h3 twm-card-s2 anm"
-                  data-speed-x="-4"
-                  data-speed-scale="-25"
-                  data-speed-opacity="50"
-                  style="
-                    transform: translate3d(-24.68px, 0px, 0px) scale(0.788865)
-                      rotate(0deg);
-                    opacity: 0.57773;
-                  "
-                >
-                  <img src="img/card-s2.png" alt="" />
-                </div>
-
-                <div class="twm-graphics-h3 twm-white-dotts">
-                  <img src="img/white-dotts.png" alt="" />
-                </div>
-
-                <div
-                  class="twm-graphics-h3 twm-top-shadow anm"
-                  data-speed-x="-16"
-                  data-speed-y="2"
-                  data-speed-scale="50"
-                  data-speed-rotate="25"
-                  style="
-                    transform: translate3d(-98.72px, -7.47px, 0px)
-                      scale(1.42227) rotate(-8.41813deg);
-                    opacity: 1;
-                  "
-                >
-                  <img src="img/top-shadow.png" alt="" />
-                </div>
-
-                <div
-                  class="twm-graphics-h3 twm-bottom-shadow anm"
-                  data-speed-x="16"
-                  data-speed-y="2"
-                  data-speed-scale="20"
-                  data-speed-rotate="25"
-                  style="
-                    transform: translate3d(98.72px, -7.47px, 0px) scale(1.16891)
-                      rotate(-8.41813deg);
-                    opacity: 1;
-                  "
-                >
-                  <img src="img/bottom-shadow.png" alt="" />
-                </div>
-              </div>
-            </div>
+            </form>
+          </div>
+          <div class="twm-bnr-popular-search">
+            <span class="twm-title">Popular Searches:</span>
+            <a href="#">Freelance Writing</a>
+            ,
+            <a href="#">Babysitting</a>
+            ,
+            <a href="#">Remote Work</a>
+            ,
+            <a href="#">Pet Care</a>
+            ,
+            <a href="#">Lawn Care</a>
+            ,
+            <a href="#">Barista</a>
+            ,
+            <a href="#">Writing</a>
+            ,
+            <a href="#">Dog Walking</a>...
           </div>
         </div>
       </div>
+
+      <!--right Section-->
+      <div class="col-xl-6 col-lg-12 col-md-12">
+        <div class="twm-bnr-right-section anm" data-wow-delay="1000ms" data-speed-x="2" data-speed-y="2" style="
+                  transform: translate3d(12.34px, -7.47px, 0px) scale(1)
+                    rotate(0deg);
+                  opacity: 1;
+                ">
+          <div class="twm-graphics-h3 twm-bg-line">
+            <img src="img/bg-line.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-user twm-user">
+            <img src="img/user.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-bg-plate">
+            <img src="img/bg-plate.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-checked-plate">
+            <img src="img/checked-plate.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-blue-block">
+            <img src="img/blue-block.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-color-dotts">
+            <img src="img/color-dotts.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-card-large anm" data-speed-y="-2" data-speed-scale="-15" data-speed-opacity="50" style="
+                    transform: translate3d(0px, 7.47px, 0px) scale(0.873319)
+                      rotate(0deg);
+                    opacity: 0.57773;
+                  ">
+            <img src="img/card.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-card-s1 anm" data-speed-y="2" data-speed-scale="15" data-speed-opacity="50" style="
+                    transform: translate3d(0px, -7.47px, 0px) scale(1.12668)
+                      rotate(0deg);
+                    opacity: 0.57773;
+                  ">
+            <img src="img/card-s1.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-card-s2 anm" data-speed-x="-4" data-speed-scale="-25" data-speed-opacity="50" style="
+                    transform: translate3d(-24.68px, 0px, 0px) scale(0.788865)
+                      rotate(0deg);
+                    opacity: 0.57773;
+                  ">
+            <img src="img/card-s2.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-white-dotts">
+            <img src="img/white-dotts.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-top-shadow anm" data-speed-x="-16" data-speed-y="2" data-speed-scale="50" data-speed-rotate="25" style="
+                    transform: translate3d(-98.72px, -7.47px, 0px)
+                      scale(1.42227) rotate(-8.41813deg);
+                    opacity: 1;
+                  ">
+            <img src="img/top-shadow.png" alt="" />
+          </div>
+
+          <div class="twm-graphics-h3 twm-bottom-shadow anm" data-speed-x="16" data-speed-y="2" data-speed-scale="20" data-speed-rotate="25" style="
+                    transform: translate3d(98.72px, -7.47px, 0px) scale(1.16891)
+                      rotate(-8.41813deg);
+                    opacity: 1;
+                  ">
+            <img src="img/bottom-shadow.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+
+</html>
