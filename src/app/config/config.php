c34efbbc6c2ea4ee70e6ac0285dd3d2675372036
iamtrazy <iamtrazy@proton.me>
2023-11-15 14:50:00 +0530

    fix: added proxy_protocol support

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -8,6 +8,6 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://joblinkup.com');
+  define('URLROOT', 'https://joblinkup.duckdns.org');
   // Site Name
   define('SITENAME', 'JobLinkUp');
