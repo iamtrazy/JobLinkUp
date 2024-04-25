93a0f9e4428c9127b1dd7fcb2dd71ecb9781b65d
iamtrazy <iamtrazy@proton.me>
2024-04-25 20:00:37 +0530

    feat: candidate profile

--- src/app/views/candidates/profile.php
+++ src/app/views/candidates/profile.php
@@ -41,30 +41,47 @@ if (!empty($city) && !empty($country)) {
                     </div>
                     <div class="twm-candi-self-bottom">
                         <!-- WhatsApp button -->
-                        <a href="https://wa.me/<?php echo $data['profile']->whatsapp_url ?>" class="site-button outline-white" style="background-color: #25D366; color: #fff;">
-                            <i class="fab fa-whatsapp"></i> WhatsApp
-                        </a>
-                        <a href="https://<?php echo $data['profile']->linkedin_url ?>" class="site-button outline-white" style="background-color: #0077B5; color: #fff;">
-                            <i class="fab fa-linkedin"></i> LinkedIn
-                        </a>
+                        <?php if (isset($data['profile']->whatsapp_url)) : ?>
+                            <a href="https://wa.me/<?php echo $data['profile']->whatsapp_url ?>" class="site-button outline-white" style="background-color: #25D366; color: #fff;">
+                                <i class="fab fa-whatsapp"></i> WhatsApp
+                            </a>
+                        <?php endif; ?>
+                        <?php if (isset($data['profile']->facebook_url)) : ?>
+                            <a href="https://<?php echo $data['profile']->linkedin_url ?>" class="site-button outline-white" style="background-color: #0077B5; color: #fff;">
+                                <i class="fab fa-linkedin"></i> LinkedIn
+                            </a>
+                        <?php endif; ?>
                         <!-- Email button -->
-                        <a href="mailto:<?php echo $data['profile']->email ?>" class="site-button outline-white" style="background-color: #FFA500; color: #fff;">
-                            <i class="far fa-envelope"></i> Email
-                        </a>
+                        <?php if (isset($data['profile']->email)) : ?>
+                            <a href="mailto:<?php echo $data['profile']->email ?>" class="site-button outline-white" style="background-color: #FFA500; color: #fff;">
+                                <i class="far fa-envelope"></i> Email
+                            </a>
+                        <?php endif; ?>
                         <!-- Phone button -->
-                        <a href="tel:<?php echo $data['profile']->phone_no ?>" class="site-button outline-white" style="background-color: #bd56fc; color: #fff;">
-                            <i class="fas fa-phone"></i> Phone
-                        </a>
-                        <a href="<?php echo URLROOT . '/assets/cvs/' . $data['profile']->cv ?> " target="_blank" class="site-button secondry outline-white">Download CV</a>
+                        <?php if (isset($data['profile']->phone_no)) : ?>
+                            <a href="tel:<?php echo $data['profile']->phone_no ?>" class="site-button outline-white" style="background-color: #bd56fc; color: #fff;">
+                                <i class="fas fa-phone"></i> Phone
+                            </a>
+                        <?php endif; ?>
+                        <!-- Download CV button -->
+                        <?php if (isset($data['profile']->cv)) : ?>
+                            <a href="<?php echo URLROOT . '/assets/cvs/' . $data['profile']->cv ?> " target="_blank" class="site-button secondry outline-white">Download CV</a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
             <h4 class="twm-s-title">About Me</h4>
-            <p><?php echo $data['profile']->about ?></p>
+            <?php if (isset($data['profile']->about)) {
+                echo '<p>' . $data['profile']->about . '</p>';
+            } else {
+                echo 'No details found';
+            } ?>
             <h4 class="twm-s-title">Keywords</h4>
             <div class="tw-sidebar-tags-wrap">
-                <div class="tagcloud">
-                    <?php
+                <?php
+                if (isset($data['profile']->keywords)) {
+                    echo '<div class="tagcloud">';
+
                     // Explode the comma-separated string into an array
                     $keywords = explode(',', $data['profile']->keywords);
 
@@ -75,19 +92,25 @@ if (!empty($city) && !empty($country)) {
                         // Output the <a> tag with the keyword
                         echo '<a>' . $keyword . '</a>';
                     }
-                    ?>
-                </div>
+
+                    echo '</div>';
+                } else {
+                    echo '<p>No keywords found</p>';
+                }
+                ?>
             </div>
         </div>
     </div>
     <div class="col-lg-4 col-md-12 rightSidebar">
         <div class="side-bar-2">
-            <div class="twm-s-map mb-5">
-                <h4 class="section-head-small mb-4">Location</h4>
-                <div class="twm-s-map-iframe">
-                    <iframe height="270" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.8534521658976!2d-118.2533646842856!3d34.073270780600225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c6fd9829c6f3%3A0x6ecd11bcf4b0c23a!2s<?= urlencode($address) ?>!5e0!3m2!1sen!2sin"></iframe>
+            <?php if ($address !== 'Unknown') : ?>
+                <div class="twm-s-map mb-5">
+                    <h4 class="section-head-small mb-4">Location</h4>
+                    <div class="twm-s-map-iframe">
+                        <iframe height="270" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.8534521658976!2d-118.2533646842856!3d34.073270780600225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c6fd9829c6f3%3A0x6ecd11bcf4b0c23a!2s<?= urlencode($address) ?>!5e0!3m2!1sen!2sin"></iframe>
+                    </div>
                 </div>
-            </div>
+            <?php endif; ?>
             <div class="twm-s-info-wrap mb-5">
                 <h4 class="section-head-small mb-4">Profile Info</h4>
                 <div class="twm-s-info">
@@ -96,34 +119,54 @@ if (!empty($city) && !empty($country)) {
                             <div class="twm-s-info-inner">
                                 <i class="fas fa-venus-mars"></i>
                                 <span class="twm-title">Gender</span>
-                                <div class="twm-s-info-discription"><?php echo ucfirst($data['profile']->gender) ?>
-                                </div>
+                                <div class="twm-s-info-discription"><?php echo ucfirst($data['profile']->gender) ?></div>
                             </div>
                         </li>
                         <li>
-                            <div class="twm-s-info-inner">
-                                <i class="fas fa-mobile-alt"></i>
-                                <span class="twm-title">Phone</span>
-                                <div class="twm-s-info-discription"><?php echo $data['profile']->phone_no ?> </div>
-                            </div>
+                            <?php if (isset($data['profile']->phone_no)) : ?>
+                                <div class="twm-s-info-inner">
+                                    <i class="fas fa-mobile-alt"></i>
+                                    <span class="twm-title">Phone</span>
+                                    <div class="twm-s-info-discription"><?php echo $data['profile']->phone_no ?></div>
+                                </div>
+                            <?php endif; ?>
                         </li>
                         <li>
                             <div class="twm-s-info-inner">
                                 <i class="fas fa-at"></i>
                                 <span class="twm-title">Email</span>
-                                <div class="twm-s-info-discription"><?php echo $data['profile']->email ?> </div>
+                                <div class="twm-s-info-discription"><?php echo $data['profile']->email ?></div>
                             </div>
                         </li>
                         <li>
                             <div class="twm-s-info-inner">
                                 <i class="fas fa-map-marker-alt"></i>
                                 <span class="twm-title">Location</span>
-                                <div class="twm-s-info-discription"><?php echo $address ?> </div>
+                                <div class="twm-s-info-discription"><?php echo $address ?></div>
                             </div>
                         </li>
+                        <?php if (isset($data['profile']->website)) : ?>
+                            <li>
+                                <div class="twm-s-info-inner">
+                                    <i class="fas fa-globe"></i>
+                                    <span class="twm-title">Website</sepan>
+                                    <div class="twm-s-info-discription"><?php echo '<a href="https://' . $data['profile']->website . ' class="text-blue"> Visit </a>' ?> </div>
+                                </div>
+                            </li>
+                        <?php endif; ?>
+                        <?php if (isset($data['profile']->age)) : ?>
+                            <li>
+                                <div class="twm-s-info-inner">
+                                    <i class="fas fa-birthday-cake"></i>
+                                    <span class="twm-title">Age</span>
+                                    <div class="twm-s-info-discription"><?php echo $data['profile']->age ?></div>
+                                </div>
+                            </li>
+                        <?php endif; ?>
                     </ul>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
