1e409f7a124ee3332dc434ba391d85e5fca0536a
iamtrazy <iamtrazy@proton.me>
2024-04-26 10:45:57 +0530

    feat: delete job

--- src/app/views/recruiters/manage.php
+++ src/app/views/recruiters/manage.php
@@ -51,11 +51,10 @@
                                             <div class="twm-table-controls">
                                                 <ul class="twm-DT-controls-icon list-unstyled">
                                                     <li>
-                                                        <button title="Edit Job" type="button" data-bs-toggle="tooltip" data-bs-placement="top">
-                                                            <a href="<?php echo URLROOT . '/recruiters/editjob/' . $job->id ?>">
-                                                                <span class="fa fa-edit"></span>
-                                                            </a>
+                                                        <button title="Edit Job" type="button" data-bs-toggle="tooltip" data-bs-placement="top" class="edit-job" data-job-id="<?php echo $job->id ?>">
+                                                            <span class="fa fa-edit"></span>
                                                         </button>
+
                                                     </li>
                                                     <li>
                                                         <button class="delete-job" title="Remove Job" data-bs-toggle="tooltip" data-bs-placement="top" data-job-id="<?php echo $job->id ?>">
@@ -132,65 +131,16 @@
                 }
             });
         });
-    });
-</script>
-<script>
-    $(document).ready(function() {
-        // Function to handle job deletion
-        $('.delete-job').on('click', function(e) {
+
+        // Function to handle click on "Edit Job" button
+        $('.edit-job').on('click', function(e) {
             e.preventDefault();
             var jobId = $(this).data('job-id');
-
-            // Show confirmation dialog using SweetAlert2
-            Swal.fire({
-                title: 'Are you sure?',
-                text: 'You will not be able to recover this job!',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    // If user confirms deletion, send AJAX request to delete the job
-                    $.ajax({
-                        url: '<?php echo URLROOT . '/recruiters/deletejob/' ?>' + jobId,
-                        type: 'POST',
-                        dataType: 'json',
-                        success: function(response) {
-                            if (response.status === 'success') {
-                                // If job is deleted successfully, show success message
-                                Swal.fire(
-                                    'Deleted!',
-                                    response.message,
-                                    'success'
-                                ).then(() => {
-                                    // Reload the page after successful deletion
-                                    location.reload();
-                                });
-                            } else {
-                                // If deletion fails, show error message
-                                Swal.fire(
-                                    'Error!',
-                                    response.message,
-                                    'error'
-                                );
-                            }
-                        },
-                        error: function(xhr, status, error) {
-                            console.error("Error deleting job:", error);
-                            // Show generic error message if AJAX request fails
-                            Swal.fire(
-                                'Error!',
-                                'Failed to delete job. Please try again later.',
-                                'error'
-                            );
-                        }
-                    });
-                }
-            });
+            // Redirect to the edit job page
+            window.location.href = '<?php echo URLROOT . '/recruiters/editjob/' ?>' + jobId;
         });
     });
 </script>
+
 
 <?php require APPROOT . '/views/inc/recruiter_footer.php'; ?>
