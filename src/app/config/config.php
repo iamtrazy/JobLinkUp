f3487adc76b8e98d20380d0349231e85e19ab8d9
heittre <116910122+heittre@users.noreply.github.com>
2024-04-27 15:46:38 +0530

    changed the env file

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -1,16 +1,16 @@
 <?php
-  // DB Params
-  define('DB_HOST', 'db');
-  define('DB_USER', 'root');
-  define('DB_PASS', '123456');
-  define('DB_NAME', 'JobLinkUp');
+  // // DB Params
+  // define('DB_HOST', 'db');
+  // define('DB_USER', 'root');
+  // define('DB_PASS', '123456');
+  // define('DB_NAME', 'JobLinkUp');
 
-// // $password = $_ENV['MYSQL_ROOT_PASSWORD'];
-// // DB Params
-// define('DB_HOST', 'db');
-// define('DB_USER', 'root');
-// define('DB_PASS', $password);
-// define('DB_NAME', 'JobLinkUp');
+$password = $_ENV['MYSQL_ROOT_PASSWORD'];
+// DB Params
+define('DB_HOST', 'db');
+define('DB_USER', 'root');
+define('DB_PASS', $password);
+define('DB_NAME', 'JobLinkUp');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
